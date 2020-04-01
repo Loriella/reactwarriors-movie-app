e69c602222c2147cf1166072842b06a60f9eda83
Loriella <loris.orlova@gmail.com>
2020-04-01 11:53:28 +0300

    Updates

---
+++ precache-manifest.7b8a2090c0f18bdb01ff8c3f7bd0434e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21dbbf3d558a571e150636992e61a7bb",
+    "url": "/reactwarriors-movie-app/index.html"
+  },
+  {
+    "revision": "1fea15467aae632cd7a5",
+    "url": "/reactwarriors-movie-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c8f253eef3340995a1d8",
+    "url": "/reactwarriors-movie-app/static/css/main.fd36d509.chunk.css"
+  },
+  {
+    "revision": "1fea15467aae632cd7a5",
+    "url": "/reactwarriors-movie-app/static/js/2.7fdf4572.chunk.js"
+  },
+  {
+    "revision": "67cd7a86caa59c4b38043aba94323370",
+    "url": "/reactwarriors-movie-app/static/js/2.7fdf4572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f253eef3340995a1d8",
+    "url": "/reactwarriors-movie-app/static/js/main.84971573.chunk.js"
+  },
+  {
+    "revision": "d5e31d09b99d4a520913",
+    "url": "/reactwarriors-movie-app/static/js/runtime-main.fe9c4360.js"
+  }
+]);
